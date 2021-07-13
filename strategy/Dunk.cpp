bcec94cff2f1cf5b2a3d7bd2c03a048cb024550c
liuwangbo03 <liuwangbo03@kuaishou.com>
2021-07-13 10:52:55 +0800

    [strategy] this pattern finished

--- strategy/Dunk.cpp
+++ strategy/Dunk.cpp
@@ -2,3 +2,44 @@
 // Created by liuwangbo on 2021/7/12.
 //
 
+#include "Dunk.h"
+
+/*
+ * 实现具体的"算法"
+ */
+void FlyWithWings::fly() {}
+
+void FlyNoWay::fly() {}
+
+void Quack::quack() {}
+
+void Squeak::quack() {}
+
+void MuteQuack::quack() {}
+
+/*
+ * 公共方法
+ */
+void Dunk::display() {}
+
+void Dunk::swim() {}
+
+/*
+ * where magic happen
+ */
+void Dunk::performQuack() {
+    quackBehavior->quack();
+}
+
+void Dunk::performFly() {
+    flyBehavior->fly();
+}
+
+void Dunk::setFlyBehavior(FlyBehavior *flyBehavior) {
+    this->flyBehavior = flyBehavior;
+}
+
+void Dunk::setQuackBehavior(QuackBehavior *quackBehavior) {
+    this->quackBehavior = quackBehavior;
+}
+
--- strategy/Dunk.h
+++ strategy/Dunk.h
@@ -8,40 +8,51 @@
 #endif //CPPPATTERNS_DUNK_H
 
 /*
- * interface
+ * 接口
  */
 class FlyBehavior{
 public:
     virtual void fly() = 0;
 };
 
+/*
+ * 被包装好的"算法"
+ */
 class FlyWithWings: public FlyBehavior{
-
+public:
+    void fly();
 };
 
 class FlyNoWay: public FlyBehavior{
-
+public:
+    void fly();
 };
 
 
 /*
- * interface
+ * 接口
  */
 class QuackBehavior{
 public:
     virtual void quack() = 0;
 };
 
+/*
+ * 被包装好的"算法"
+ */
 class Quack: public QuackBehavior{
-
+public:
+    void quack();
 };
 
 class Squeak: public QuackBehavior{
-
+public:
+    void quack();
 };
 
 class MuteQuack: public QuackBehavior{
-
+public:
+    void quack();
 };
 
 /*
@@ -51,24 +62,40 @@ class Dunk{
 public:
     virtual void swim();
     virtual void display();
+
+    // 策略模式相关方法
+    // 使用"算法"
     virtual void performQuack();
     virtual void performFly();
-    virtual void setFlyBehavior();
-    virtual void setQuackBehavior();
+    // 动态配置"算法"
+    virtual void setFlyBehavior(FlyBehavior *flyBehavior);
+    virtual void setQuackBehavior(QuackBehavior *quackBehavior);
+
+private:
+    FlyBehavior *flyBehavior;
+    QuackBehavior *quackBehavior;
 };
 
 class MallardDuck: public Dunk{
-
+public:
+    void swim();
+    void display();
 };
 
 class RedHeadDuck: public Dunk{
-
+public:
+    void swim();
+    void display();
 };
 
 class RubberDuck: public Dunk{
-
+public:
+    void swim();
+    void display();
 };
 
 class DecoyDuck: public Dunk{
-
+public:
+    void swim();
+    void display();
 };
