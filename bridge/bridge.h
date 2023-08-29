925c906d3664fe1eb14c84d8fc3b80b809959953
liuwangbo03 <liuwangbo03@kuaishou.corp.com>
2023-08-29 11:07:56 +0800

    [bridge] bridge pattern

--- bridge/bridge.h
+++ bridge/bridge.h
@@ -1,7 +1,28 @@
 #pragma once
-
+#include <string>
 namespace cdp {
 namespace bridge {
+
+/**
+ * 这个设计模式的诀窍在于将具体实现隐藏到.cc文件而非.h文件中，
+ * 好处是将具体实现彻底隐藏起来；如果将实现定义到头文件中，
+ * 实现的信息实际还是会暴露出来，另外这么做对二进制兼容实际上
+ * 也更方便，也对提高编译速度有帮助
+ * 
+*/
+
+class Person {
+ public:
+  std::string name;
+  void greet();
+  Person();
+  ~Person();
+
+ private:
+  // 前向声明Person的实现
+  class PersonImpl;
+  PersonImpl* impl;
+};
 
 } // namespace bridge
 } // namespace cdp
--- bridge/brige.cc
+++ bridge/brige.cc
@@ -1,1 +1,20 @@
-// holder
+#include "bridge.h"
+
+namespace cdp {
+namespace bridge {
+
+class Person::PersonImpl {
+ public:
+  void greet(const Person* p) {
+    // implement this
+  }
+};
+
+Person::Person() : impl(new PersonImpl) {}
+
+Person::~Person() { delete impl; }
+
+void Person::greet() { impl->greet(this); }
+
+} // namespace bridge
+} // namespace cd
